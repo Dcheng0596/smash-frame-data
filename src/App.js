896d602ea18641e67b1e3cc07b44dbbeff745568
Darien Cheng <dcheng0596@bths.edu>
2019-09-20 22:19:48 -0400

    Added background body styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
     </div>
   );
 }
