f5eaa88bd9768f097f8b139bfadfafb9c21eef2d
Darien Cheng <dcheng0596@bths.edu>
2019-09-23 12:36:53 -0400

    Update Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Search from './Search';
+import FighterList from './FighterList'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Grid() {
     return(
-      <Container className="text-warning font-weight-bold">
-        <h1 class="font-weight-bold text-center">Choose Your Fighter!</h1>
+      <Container className="fluid text-warning font-weight-bold">
+        <h1 className="font-weight-bold text-center">Choose Your Fighter!</h1>
         <Row>
           <Col>
             <Search/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FighterList/>
           </Col>
         </Row>
       </Container>
